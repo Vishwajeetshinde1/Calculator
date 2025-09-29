2b4395cb9a2205530edab3a73f3641c25caf2472
Vishwajeet Shinde <159220090+Vishwajeetshinde1@users.noreply.github.com>
2025-09-29 19:32:11 +0530

    update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = '';
+let clickSound = new Audio('button.mp3');
+
+Array.from(buttons).forEach(function(btn) {
+  btn.addEventListener('click', (e) => {
+    clickSound.currentTime = 0; 
+    clickSound.play();
+
+    let value = e.target.innerHTML;
+
+    if (value === 'Del') {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+
+    } else if (value === 'AC') {
+      string = '';
+      display.value = string;
+
+    } else if (value === '=') {
+      let expression = string.replace(/÷/g, '/').replace(/x/g, '*');
+      string = eval(expression);
+      display.value = string;
+
+    } else {
+      string += value;
+      display.value = string;
+    }
+  });
+});
